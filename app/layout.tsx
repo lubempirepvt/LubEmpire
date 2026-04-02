be0d7e8a32b01e6c79159a54c5077814ddea37f7
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-04-02 11:06:10 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
+        suppressHydrationWarning
       >
         {children}
       </body>
